4a05e3f1fd4b07fb1e81493a96fc9a6ef2422952
agustin <agustin@ubuntu.myguest.virtualbox.org>
2023-06-14 19:54:19 -0300

    added the option to use parameters to the script. Also the script now show both ggraphics at once

--- LotkaVolterra.py
+++ LotkaVolterra.py
@@ -1,5 +1,5 @@
 import math
-
+import argparse
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -23,19 +23,48 @@ def d(b,m,t,hashmapP,hashmapD):
     return round((b*hashmapP[round((t),5)]*hashmapD[round((t),5)]-m*hashmapD[round((t),5)]),5)
 
 def main():
+    parser = argparse.ArgumentParser()
     r=3                       #tasa de crecimiento de presas en ausencia de depredadores
     a=2                       #coeficiente de depredacion
-    b=1                       #tasa de cercimiento del numero de depredadores
+    b=1                       #tasa de crecimiento del numero de depredadores
     m=2                       #tasa de mortalidad de depredadores
     intervalInit=0  
     intervalEnd=50
     intialValueP=2
     intialValueD=2
+    h=0.01
+    parser.add_argument("-i", "--interval", type=float, help="Interval max. Default = 50. The sistem will also stop when the amount of preys or depredators reach less than 0.005")
+    parser.add_argument("-s", "--step", type=float, help="Size of the step. Default = 0.01")
+    parser.add_argument("-d", "--depredator", type=int, help="Starting amount of depredators. Default = 2")
+    parser.add_argument("-p", "--prey", type=int, help="Starting amount of preys. Default 2 = 2")
+    parser.add_argument("-r", type=float, help="Growth rate of prey in the absence of predators. Default = 3")
+    parser.add_argument("-a", type=float, help="Predation coefficient. Default = 2")
+    parser.add_argument("-b", type=float, help="Growth rate of the number of predators. Default = 1")
+    parser.add_argument("-m", type=float, help="Predator mortality rate. Default = 2")
+
+    args = parser.parse_args()
+
+    if args.interval:
+        intervalEnd=args.interval
+    if args.step:
+        h=args.step
+    if args.depredator:
+        intialValueD=args.depredator
+    if args.prey:
+        intialValueP=args.prey
+    if args.r:
+        r=args.r
+    if args.a:
+        a=args.a
+    if args.b:
+        b=args.b
+    if args.m:
+        m=args.m
+    
     hashmapP = {intervalInit:intialValueP}
     hashmapD = {intervalInit:intialValueD}
-    h=0.01
+
     eulerAlgorithm(h,r,a,b,m,intervalEnd,hashmapP,hashmapD,p,d)
-    
     # Print values
     for clave in hashmapP:
         valor= hashmapP[clave]
@@ -44,25 +73,28 @@ def main():
         valor= hashmapD[clave]
         print(str(clave)+" : "+str(valor))
 
+
+    fig ,(ax1, ax2) = plt.subplots(1,2)
     # 1
-    plt.plot(list(hashmapP.values()),'-',color='g', label='Preys')
-    plt.plot(list(hashmapD.values()), '-', color= 'r', label='Depredators')
-    plt.title('Population through time')
-    plt.xlabel('Simulation time')
-    plt.ylabel('Population')
-    plt.legend()    
-    plt.show()
+    ax1.plot(list(hashmapP.values()),'-',color='g', label='Preys')
+    ax1.plot(list(hashmapD.values()), '-', color= 'r', label='Depredators')
+    ax1.set_title('Population through time')
+    ax1.set_xlabel('Simulation time')
+    ax1.set_ylabel('Population')
+    ax1.legend()    
+
 
     # 2
-    # x=hashmapP.values()
-    # y=hashmapD.values()
-    # plt.title('Interaction of the number of prey and predators')
-    # plt.xlabel('Population of depredators')
-    # plt.ylabel('Population of preys')
-    # plt.plot(x,y)
-    # plt.legend() 
-    # plt.show()
-
+    x=hashmapP.values()
+    y=hashmapD.values()
+    ax2.set_title('Interaction of the number of prey and predators')
+    ax2.set_xlabel('Population of depredators')
+    ax2.set_ylabel('Population of preys')
+    ax2.plot(x,y)
+    ax2.legend() 
+    fig.tight_layout()
+    plt.subplot(1,2,2)
+    plt.show()
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -11,4 +11,15 @@ En el presente repositorio se encuentran 2 archivos:
 - Reporte.pdf: contiene el reporte del trabajo realizado
 
 ## Uso
-   python3 LotkaVolterra.py
+   Para obtener una vista de ayuda de como utilizar los parametros opcionales se puede utilizar
+   python3 LotkaVolterra.py -h
+
+   Parámetros opcionales:
+   "-i", "--interval", help = "Interval max. Default = 50. The sistem will also stop when the amount of preys or depredators reach less than 0.005"
+   "-s", "--step", help = "Size of the step. Default = 0.01"
+   "-d", "--depredator", type = int, help = "Starting amount of depredators. Default = 2"
+   "-p", "--prey", type = int, help = "Starting amount of preys. Default = 2"
+   "-r", help = "Growth rate of prey in the absence of predators. Default = 3"
+   "-a", help = "Predation coefficient. Default = 2"
+   "-b", help = "Growth rate of the number of predators. Default = 1"
+   "-m", help = "Predator mortality rate. Default = 2"
